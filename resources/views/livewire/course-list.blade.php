43a11458ffd6293fa939eaf77042808683dbbf0c
Manuel Win10 <mapg.bit@outlook.com>
2021-02-10 05:31:02 -0600

    11.- Livewire: diseño del componente

--- resources/views/livewire/course-list.blade.php
+++ resources/views/livewire/course-list.blade.php
@@ -1,5 +1,13 @@
-<div>
+<div class="grid grid-cols-3 gap-4 mt-8">
     @foreach($courses as $course)
-        {{ $course->id }}
+    <div class="ng-white shadow-lg rounded-lg px-4 py-6 text-center">
+        <a href="#">´
+            <img src="{{ $course->image }}" alt="" class="rounded-md mb-2">
+        </a>
+        <h2 class="text-lg text-gray-600 truncate uppercase">{{ $course->name }}</h2>
+        <h3 class="text-lg text-gray-500">{{ $course->excerpt }}</h3>
+
+        <img src="{{ $course->user->avatar }}" alt="" class="rounded-full mt-4 mx-auto h-16 w-16">
+    </div>
     @endforeach
 </div>
