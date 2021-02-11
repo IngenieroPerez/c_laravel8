5c6911ee5c680791af2251e5791caae937022e8b
Manuel Win10 <mapg.bit@outlook.com>
2021-02-10 18:11:55 -0600

    15.- Cursos similares

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -12,8 +12,23 @@ class Course extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+    
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
 
     public function getExcerptAttribute(){
         return substr($this->description, 0, 80) . "...";
+    }
+
+    public function similar(){
+        return $this->where([
+            ['category_id', $this->category_id],
+            ['id', '!=', $this->id]
+        ])
+        ->with('user')
+        ->inRandomOrder()
+        ->take(2)
+        ->get();
     }
 }
